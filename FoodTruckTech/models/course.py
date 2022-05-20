d5cb206706983ad52da32d3fbedefb33f365d8c7
Jarvis Dumas <99155980+Captivea-JDU@users.noreply.github.com>
2022-05-20 10:51:57 -0400

    Update course.py

--- FoodTruckTech/models/course.py
+++ FoodTruckTech/models/course.py
@@ -7,13 +7,4 @@ class Course(models.Model):
     _name = 'food.course'
     _description = 'Course Info'
     
-    name = fields.Char(string='Title', required=True)
-    desription = fields.Text(string='Description')
-    
-    level = fields.Selection(string='Level',
-                            selection=[('beginner', 'Beginner')
-                                      ('intermediate', 'Intermediate'),
-                                      ('advanced', 'Advanced')],
-                            copy=False)
-    
-    active = fields.Boolean(string='Active', default=True)
+  
