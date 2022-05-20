e32bafa3247b6269dc2113432491cb568632021e
Jarvis Dumas <99155980+Captivea-JDU@users.noreply.github.com>
2022-05-20 09:54:22 -0400

    update

---
+++ FoodTruckTech/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'FoodTruck Tech',
+    
+    'summary': """Academy app to manage Training""",
+    
+    'description': """
+    Academy module to manage training:
+    - Course
+    - Sessions
+    - Attendees
+    """,
+    
+    'author': 'Odoo',
+    
+    'website': 'www.Odoo.com',
+    'category': 'Training',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        
+        
+    ],
+    
+    'demo': [
+        
+        
+    ],
+}
