dba8c40a60dd576db5dd070a40a278ce4d408fe8
Jarvis Dumas <99155980+Captivea-JDU@users.noreply.github.com>
2022-05-20 11:59:36 -0400

    Update sale_order.py

--- FoodTruckTech/models/sale_order.py
+++ FoodTruckTech/models/sale_order.py
@@ -5,11 +5,8 @@ from odoo import models, fields, api
 
 class SaleOrder(models.Model):
 
-
-
     _inherit = "sale.order"
-	recipe = fields.Char(string='Recipe')
-    record['x_studio_email'] = record.partner_id.email
+    self['x_studio_email'] = self.partner_id.email
 
     return record
 
