d387b0de8b8a5594057d18ecbf5d468cbf44762b
Jarvis Dumas <99155980+Captivea-JDU@users.noreply.github.com>
2022-05-20 10:42:07 -0400

    Update course.py

--- FoodTruckTech/models/course.py
+++ FoodTruckTech/models/course.py
@@ -1,4 +1,3 @@
-
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
@@ -7,3 +6,14 @@ class Course(models.Model):
     
     _name = 'food.course'
     _description = 'Course Info'
+    
+    name = fields.Char(string='Title', required=True)
+    desription = fields.Text(string='Description', required=True)
+    
+    level = fields.Selection(string='Level',
+                            selection=[('beginner', 'Beginner')
+                                      ('intermediate', 'Intermediate'),
+                                      ('advanced', 'Advanced')]
+                            copy=False)
+    
+    active = fields.Boolean(string='Active', default=True)
