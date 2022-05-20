8771abd417be9c27204e41e9422af7cbd4db959c
Jarvis Dumas <99155980+Captivea-JDU@users.noreply.github.com>
2022-05-20 11:34:45 -0400

    Create sale_order.py

---
+++ FoodTruckTech/models/sale_order.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class Actions(models.Model):
+	class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    record['x_studio_email'] = record.partner_id.email
+
+    return record
+
