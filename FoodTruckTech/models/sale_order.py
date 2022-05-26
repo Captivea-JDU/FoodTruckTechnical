ea88d267f89c636681ef413a4a3a5719fa71ca44
Jarvis Dumas <99155980+Captivea-JDU@users.noreply.github.com>
2022-05-26 10:28:50 -0400

    deleting stuff

--- FoodTruckTech/models/sale_order.py
+++ FoodTruckTech/models/sale_order.py
@@ -1,15 +1,2 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-
-class SaleOrder(models.Model):
-
-    _inherit = "sale.order"
-    self['x_studio_email'] = self.partner_id.email
-
-
-
-
 
 
